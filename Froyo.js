026a73533f083636c8a80dccc3195053dd6192f6
Blake <bpatin77@gmail.com>
2024-09-11 17:56:50 -0500

    From line 10-21 I created a new object and used a for loop to loop through the array and called a new variable str inorder to differentiate between the different flavors. In the if/else statement I would add 1 if the array finds that a value is defined and created a table with the total findings

--- Froyo.js
+++ Froyo.js
@@ -3,6 +3,10 @@ const userInputString = prompt(
     "vanilla,vanilla,vanilla,strawberry,coffee,coffee"
   );
 
+  // Splits the above string of flavers into an own array of substrings
+  const userInputArray = userInputString.split(','); //userInputString.split(',') is telling the code to create a substring after each comma on line 2-3
+  console.log(userInputArray);
+  
 
 
 
