638d99ee893c1b4e106ffd76b620f2ae377c50d9
Blake <bpatin77@gmail.com>
2024-09-11 17:47:42 -0500

    From line 6-9 I split the code from line 1-4 into an array of substrings

---
+++ Froyo.js
@@ -0,0 +1,9 @@
+const userInputString = prompt(
+    "Please enter the Froyo flavors you would like to order seperated by commas.",
+    "vanilla,vanilla,vanilla,strawberry,coffee,coffee"
+  );
+
+
+
+
+
